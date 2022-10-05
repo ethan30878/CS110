6b6909e34f18ea31adcb08bcfdb317d45a3a4f17
Ethan30878 <ethan30878@gmail.com>
2022-10-04 21:24:42 -0400

    Final(?)

--- ROT-n Cipher/src/Project1.java
+++ ROT-n Cipher/src/Project1.java
@@ -1,7 +1,7 @@
 /* Name: Ethan Glenn
 * File Name: Project1.java
 * Date: 7 October, 2022
-* Program Description: Beep boop bop
+* Program Description: The point of this project is to create a program that encrypts user input or a file using a ROT-n algorithm.
 */
 import javax.swing.JOptionPane;
 import java.io.BufferedReader;
@@ -32,25 +32,18 @@ public class Project1 {
 	/* This function asks the user where to get the data from, a string or a file. */
 	public static void queryPlainText() throws IOException {
 		boolean validInput = false;
-		
 		do {
 			String option = JOptionPane.showInputDialog(null, WELCOME_MSG, "ROT-n Cipher", JOptionPane.INFORMATION_MESSAGE);
-			
 			switch (option) {
-
-				/* Option '1' is from user text. */
 				case "1":
 					_plainText = JOptionPane.showInputDialog(null, "Enter the plain text:", "ROT-n Cipher", JOptionPane.INFORMATION_MESSAGE);
 					getNumberOfRotations();
 					validInput = true;
 					break;
-
-				/* Option '2' is from a file path. */
 				case "2":
-					/* Get the filename from the user. */
+					/* Gets the filename from the user. */
 					String fileName = JOptionPane.showInputDialog(null, "Enter the name of the file: ", "ROT-n Cipher", JOptionPane.INFORMATION_MESSAGE);
-
-					/* Attempt to open the file. */
+					/* Attempts to open the file. */
 					FileReader reader = null;
 					try {
 						reader = new FileReader(fileName);
@@ -58,64 +51,35 @@ public class Project1 {
 						JOptionPane.showMessageDialog(null, "File not found: \"" + fileName + "\"\nPlease try again.");
 						continue;
 					}
-
-					/* Read the file to the plain text string. */
+					/* Reads the file to the plain text string. */
 					BufferedReader in = new BufferedReader(reader);
 					while (in.ready()) {
 						_plainText += in.readLine() + "\n";
 					}
 					in.close();
 					reader.close();
-	
 					getNumberOfRotations();
 					validInput = true;
-				break;
-
+					break;
 				default:
+					/* Displays an error message if choice is not 1 or 2. */
 					JOptionPane.showMessageDialog(null, "Invalid choice: " + option + "\nMust be between 1 and 2.\nPlease try again.");
 					validInput = false;
 			}
 		} while (!validInput);  
 	}
-
-	// /* This function encodes the data using the ROT-N algorithm. */
-	// public static String encryptRotN(String str) { 	
-	// 	/* The user string will be in upper case to keep algorithm incomplex. */	
-	// 	str = str.toUpperCase();
-		
-	// 	/* Rotate the ASCII english characters and nothing more. */
-	// 	String cipherText = "";
-
-	// 	/* Run the encoder. */
-	// 	for (int i = 0; i < str.length(); i++) {
-	// 		char c = str.charAt(i);
-
-	// 		if (Character.isAlphabetic(c)) {
-	// 			c = (char)((((char)(c - 65) + _n) % 26) + 65);
-	// 			cipherText += c;
-	// 		} else {
-	// 			cipherText += c;
-	// 		}
-	// 	}
-
-	// 	/* Return the newly encrypted string. */
-	// 	return cipherText;
-	// }
-
-	/*  */
+	/* Acquires and validates n value for ROT-n calculations. */
 	public static void getNumberOfRotations() {
-		/* If the text that was inserted was incorrect do this process again. */
 		boolean valid = false;
 		do {
-			/* Retrieve the number of rotations from the user. */
+			/* Retrieves the number of rotations from the user. */
 			String value = JOptionPane.showInputDialog(null, ENTER_VALUE);
-			
-			/* Attempt to convert the string to an integer. */
+			/* Attempts to convert the string to an integer. */
 			try {
 				_n = Integer.parseInt(value);
 			} catch(NumberFormatException Exception) {
 				/* Displays an error if this string could not be converted to an integer. */
-				String notNumeric = String.format("Value is not numeric: %d\nTry again.", value);
+				String notNumeric = String.format("Value is not numeric: %s\nTry again.", value);
 				JOptionPane.showMessageDialog(null, notNumeric);
 				continue;
 			}
@@ -123,45 +87,47 @@ public class Project1 {
 			if (_n >=1  && _n <= 25) {
 				break;
 			} else {
+				/* Displays an error if n is out of range. */
 				String outOfRange = String.format("Invalid number: %d\nMust be between 1 and 25.\nPlease try again.", _n);
 				JOptionPane.showMessageDialog(null, outOfRange);
 				valid = true;
 			}
-		} while(valid);
+		} while(!valid);
 	}
 	/* Main method, contains basic instructions for entire program */
 	public static void queryOutput() throws IOException {
-		
-		String outputOption = JOptionPane.showInputDialog(null, OUTPUT_MSG);
 		boolean valid = false;
 		do 	 {
+			String outputOption = JOptionPane.showInputDialog(null, OUTPUT_MSG);
+			/* Checks output decision from user */
 			switch (outputOption) {
 				case "1":
+					/* Prints cipherText to screen. */
 					JOptionPane.showMessageDialog(null, _cipherText);
 					valid = true;
 					break;
 				case "2":
+					/* Writes cipherText to file.  */
 					BufferedWriter out = new BufferedWriter(new FileWriter("riddles-encrypted.txt"));
 					out.write(_cipherText);
 					out.close();
 					valid = true;
 					break;
 				default:
+					/* Displays an error message if choice is not 1 or 2.*/
 					JOptionPane.showMessageDialog(null, "Invalid choice: " + outputOption + "\nMust be between 1 and 2.\nPlease try again.");
+					break;
 			}
 		} while(!valid);
 	}
+	/* Runs entire program and loops if user wishes to run again. */
 	public static void main(String[] args) throws Exception {
 		boolean runAgain = true;
 		int counter = 0;
 		do {
 			queryPlainText();
-			// _cipherText = encryptRotN(_plainText);
-
-			/* The user string will be in upper case to keep algorithm incomplex. */	
 			_plainText = _plainText.toUpperCase();
-
-			/* Run the encoder. */
+			/* Runs the encoder. */
 			for (int i = 0; i < _plainText.length(); i++) {
 				char c = _plainText.charAt(i);
 				if (Character.isAlphabetic(c)) {
@@ -174,9 +140,8 @@ public class Project1 {
 			}
 			queryOutput();
 			JOptionPane.showMessageDialog(null, "You have successfully encrypted " + counter + " characters.");
-		
+			/* Takes user decision to run again or not. */
 			int yesOrNo = JOptionPane.showConfirmDialog(null, "Encrypt another plain text?");
-			
 			if (yesOrNo == JOptionPane.NO_OPTION || yesOrNo == JOptionPane.CANCEL_OPTION)
 				runAgain = false;
 		} while(runAgain);		
